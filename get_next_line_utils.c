eab8377578de6b830535dab4b7cdd6edf1046dcf
jerryhp <horjarry@gmail.com>
2025-01-06 22:33:24 +0800

    first

---
+++ get_next_line_utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhor <jhor@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 14:13:42 by jhor              #+#    #+#             */
+/*   Updated: 2025/01/06 14:13:42 by jhor             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+size_t  ft_strlen(const char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i] != '\0')
+    {
+        i++;
+    }
+    return (str[i]);
+}
+
+// int main()
+// {
+    // char *str1 = "Hello there";
+// 
+    // printf("%s", str1);
+// }
+
+char	*ft_strchr(const char *s, int c)
+{
+    c = ((unsigned char)c);
+
+    while (*s != '\0')
+    {
+        if (*s == c)
+            return ((char *)s);
+        s++;
+    }
+    return (NULL);
+}
+// 
+// int main()
+// {
+    // char *str = "Hello world";
+// 
+    // printf("%s", ft_strchr(str, 'o'));
+// }
+
+char	*ft_strjoin(char *line, char *buff)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	if (!line)
+	{
+		line = (char *)malloc(1 * sizeof(char));
+		line[0] = '\0';
+	}
+	if (!line || !buff)
+		return (NULL);
+	str = malloc(sizeof(char) * ((ft_strlen(line) + ft_strlen(buff)) + 1));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	j = 0;
+	if (line)
+		while (line[++i] != '\0')
+			str[i] = line[i];
+	while (buff[j] != '\0')
+		str[i++] = buff[j++];
+	str[ft_strlen(line) + ft_strlen(buff)] = '\0';
+	free(line);
+	return (str);
+}
